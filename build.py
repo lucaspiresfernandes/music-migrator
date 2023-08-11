2e3d75ef76cfca328bb0d1c4ffcbfa58e935e42a
Alyssa Fernandes <alyssapiresfernandes@gmail.com>
2023-08-11 13:18:10 -0400

    Update build.py

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os
 
-ytMusicApiLocalePath = 'C:/Python311/Lib/site-packages/ytmusicapi/locales;ytmusicapi/locales'
+
+ytMusicApiLocalePath = str(
+    f'C:/Python311/Lib/site-packages/ytmusicapi/locales{os.pathsep}ytmusicapi/locales')
 
 os.system(
     f'python -m PyInstaller --onefile ./migrator.py --add-data "{ytMusicApiLocalePath}"')
